9be53faaa8b98d9f9955be16a28bc47e1d1e9cb9
ParthibBr <bparthib77@gmail.com>
2023-09-24 09:50:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const course = require("./data/course.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello Server!");
+});
+
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.listen(port, () => {
+  console.log(`Api is running on port ${port}`);
+});
